63843c6c4b7c5fc98e958d2dd11b7939ffe2fc28
github username <github email address>
2019-09-02 10:46:21 -0400

    Done.

--- src/components/PokemonCollection.js
+++ src/components/PokemonCollection.js
@@ -1,15 +1,21 @@
-import React from 'react'
-import PokemonCard from './PokemonCard'
-import { Card } from 'semantic-ui-react'
+import React from "react";
+import PokemonCard from "./PokemonCard";
+import { Card } from "semantic-ui-react";
 
 class PokemonCollection extends React.Component {
+  // now that this componenet has ALL of the pokemon obj's from the index component...I want to create a method that will
+  // Create a pokemon card and give each card it's appropraite obj to use to render info to the card.
+  createCard = () => {
+    return this.props.pokemon.map(poke => {
+      return <PokemonCard details={poke} key={poke.name} />;
+    });
+  };
+
   render() {
-    return (
-      <Card.Group itemsPerRow={6}>
-        <h1>Hello From Pokemon Collection</h1>
-      </Card.Group>
-    )
+    return <Card.Group itemsPerRow={6}>{this.createCard()}</Card.Group>;
   }
 }
 
-export default PokemonCollection
+export default PokemonCollection;
+
+//this.props.pokemon.each => render a pokecard within a div or something
--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -1,23 +1,65 @@
-import React from 'react'
-import PokemonCollection from './PokemonCollection'
-import PokemonForm from './PokemonForm'
-import { Search } from 'semantic-ui-react'
-import _ from 'lodash'
+import React from "react";
+import PokemonCollection from "./PokemonCollection";
+import PokemonForm from "./PokemonForm";
+import { Search } from "semantic-ui-react";
+import _ from "lodash";
 
-class PokemonPage extends React.Component {
+class PokemonIndex extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      allPokemon: [],
+      filteredPokemon: [],
+      searchTerm: ""
+    };
+  }
+  // once i have my fetch data set to the state, I want to give this data to the pokemon collection
+  componentDidMount() {
+    fetch("http://localhost:3000/pokemon")
+      .then(r => r.json())
+      .then(data => {
+        this.setState({ allPokemon: data, filteredPokemon: data });
+      });
+  }
+
+  onAddPokemon = event => {
+    event.persist();
+    let name = event.target[0];
+    let hp = event.target[1];
+    let front = event.target[2];
+    let back = event.target[3];
+  };
+  // I want to use my fetched pokemon data and set it to state to be rendered to the DOM
+  handleSearchInput = (e, { value }) => {
+    let x = this.state.allPokemon.filter(poke => {
+      return poke.name.includes(value);
+    });
+    this.setState({
+      filteredPokemon: x
+    });
+  };
+
   render() {
     return (
       <div>
         <h1>Pokemon Searcher</h1>
         <br />
-        <Search onSearchChange={_.debounce(() => console.log('🤔'), 500)} showNoResults={false} />
+        <Search
+          onSearchChange={_.debounce(this.handleSearchInput, 500)}
+          showNoResults={false}
+        />
         <br />
-        <PokemonCollection />
+        <PokemonForm onAddPokemon={this.onAddPokemon} />
         <br />
-        <PokemonForm />
+        <PokemonCollection pokemon={this.state.filteredPokemon} />
       </div>
-    )
+    );
   }
 }
 
-export default PokemonPage
+export default PokemonIndex;
+
+// I need to give my pokemon collection actual pokemons that it in turn can give each of those to an individual pokecard
+// By default i would like all of the pokemon to show when the page loads
+// pokemon index is going to hold the majority of my state
+// I need to fetch all of the pokemon
