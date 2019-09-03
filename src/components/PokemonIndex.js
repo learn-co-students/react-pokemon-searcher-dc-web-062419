c947c7ed78052cf3447e232dfbe1367d77ec2289
Luke O'Malley <Lukeomalley95@gmail.com>
2019-09-03 12:09:15 -0400

    Done.

--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -1,23 +1,47 @@
-import React from 'react'
-import PokemonCollection from './PokemonCollection'
-import PokemonForm from './PokemonForm'
-import { Search } from 'semantic-ui-react'
-import _ from 'lodash'
+import React from 'react';
+import PokemonCollection from './PokemonCollection';
+import PokemonForm from './PokemonForm';
+import { Search } from 'semantic-ui-react';
 
 class PokemonPage extends React.Component {
+  state = {
+    pokemon: [],
+    searchTerm: '',
+  };
+
+  componentDidMount() {
+    fetch(`http://localhost:3000/pokemon`)
+      .then(res => res.json())
+      .then(pokemon => this.setState({ pokemon }))
+      .catch(e => console.error(e));
+  }
+
+  handleSearchChange = ({ value }) => {
+    this.setState({ searchTerm: value });
+  };
+
+  filterPokemon() {
+    if (this.state.searchTerm === '') return this.state.pokemon;
+    return this.state.pokemon.filter(pokemon => pokemon.name.includes(this.state.searchTerm));
+  }
+
+  addPokemon = newPokemon => {
+    this.setState({ pokemon: [...this.state.pokemon, newPokemon] });
+  };
+
   render() {
     return (
       <div>
         <h1>Pokemon Searcher</h1>
         <br />
-        <Search onSearchChange={_.debounce(() => console.log('🤔'), 500)} showNoResults={false} />
+        <Search onSearchChange={this.handleSearchChange} showNoResults={false} />
         <br />
-        <PokemonCollection />
+        <PokemonCollection pokemon={this.filterPokemon()} />
         <br />
-        <PokemonForm />
+        <PokemonForm handleSubmit={this.addPokemon} />
       </div>
-    )
+    );
   }
 }
 
-export default PokemonPage
+export default PokemonPage;
